593856c351c24a1f30302b260ee9b5c9fd4ce254
Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>
2018-05-01 14:29:58 +0100

    Added os.path import

--- card.py
+++ card.py
@@ -10,6 +10,8 @@
 # along with this library; if not, write to the Free Software
 # Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
+import os.path
+
 from gi.repository import Gtk, Gdk, GObject, GdkPixbuf
 
 from sprites import Sprite
