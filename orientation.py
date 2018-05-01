01acca28d22fdbcd4980a52255ccfb1700767649
Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>
2018-05-01 15:02:55 +0100

    [Fix] Added GPLv3+ license repaired GPLv3+ short text in source files, remove MANIFEST.

--- orientation.py
+++ orientation.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2009-11 Walter Bender
 # Copyright (c) 2012 Ignacio Rodríguez
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License
-# along with this library; if not, write to the Free Software
-# Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 # There are a limited number of valid orientation combinations available,
--- window.py
+++ window.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2009-11 Walter Bender
 # Copyright (c) 2012 Ignacio Rodríguez
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License
-# along with this library; if not, write to the Free Software
-# Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
 from gettext import gettext as _
