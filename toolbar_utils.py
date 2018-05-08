98e149c5b72629a3633fadf0ac901d46b6fbe1d2
James Cameron <quozl@laptop.org>
2018-05-08 10:33:29 +1000

    Review changes
    - an extra gi.require_version was still present,
    
    - the copyright to Ignacio had lost that it was for the GTK+ 3 port.

--- toolbar_utils.py
+++ toolbar_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2011, Walter Bender
-# Copyright (C) Ignacio Rodriguez <ignaciorodriguez@sugarlabs.org>
+# Copyright (C) Ignacio Rodriguez <ignaciorodriguez@sugarlabs.org> (GTK+ 3)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
