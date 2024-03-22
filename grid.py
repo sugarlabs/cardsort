sugarlabs/cardsort

commit 909c9f50bb78af9dc2cab1a441bd364803a1fc7b

renamed variable

grid.py
@@ -220,7 +220,7 @@ class Grid:
 
     # Test all relevant borders, ignoring edges
     # Highlight matches.
-    def test(self):
+    def test_borders(self):
         orientation = f"{self.grid_size[0]}x{self.grid_size[1]}"
 
         if orientation == "3x3":

commit 096e82a71bb9d057e4e6602b0a131def77575712

Fix AttributeError: 'Grid' object has no attribute 'test'
909c9f5 ("renamed variable") introduced a regression.

grid.py
@@ -147,7 +147,7 @@ class Grid:
 
         if orientation == "3x3":
             self.set_grid([0, 1, 2, 3, 4, 5, 6, 7, 8])
-        
+
         elif orientation == "2x2":
             r = int(uniform(0, 4))
             if r == 0:
@@ -180,9 +180,9 @@ class Grid:
             else:
                 self.set_grid([7, 1, 3, 2, 5, 4, 8, 0, 6])
                 self.hide_list([3, 4, 6])
-                
+
         self.hide_masks()
-        self.test()
+        self.test_borders()
 
     # swap card a and card b
     # swap their entries in the grid and the position of their sprites
@@ -231,7 +231,7 @@ class Grid:
             return self.test3x2()
         elif orientation == "2x2":
             return self.test2x2()
-        
+
         return False
 
     def test3x3(self):

window.py
@@ -114,7 +114,7 @@ class Game():
             # self.grid.print_grid()
         self.press = -1
         self.release = -1
-        if self.grid.test() is True:
+        if self.grid.test_borders() is True:
             if self.sugar is True:
                 self.activity.results_label.set_text(
                     _("You solved the puzzle."))